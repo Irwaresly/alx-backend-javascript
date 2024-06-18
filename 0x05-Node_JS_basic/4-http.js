37a6c736bdea1f7198b09ad7bf03eeb2c886648a
Irwaresly <silviairware372@gmail.com>
2024-06-18 00:37:51 -0700

    nodejs10

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,20 @@
 const http = require('http');
 
+const host = '127.0.0.1';
+const port = 1245;
+
+/*
+  Create an HTTP server that listens for incoming requests.
+  The server uses a callback function to handle requests and responses.
+  - req: the request object representing the incoming request
+  - res: the response object used to send a response back to the client
+*/
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server running at http://localhost:1245/');
-});
+app.listen(port, host, () => {});
 
 module.exports = app;
