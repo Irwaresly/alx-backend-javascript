a6da7b9a0e4359f9b033ff7dff8c46633a6549d5
Irwaresly <silviairware372@gmail.com>
2024-06-18 02:50:33 -0700

    nodejs16

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,16 +1,13 @@
 const express = require('express');
+
 const app = express();
 const port = 1245;
 
-/*
-  Create a small HTTP server using Express.
-  Define a route for the root endpoint ('/').
-  When a GET request is made to '/', the server responds with 'Hello Holberton School!'.
-*/
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {});
+app.listen(port, () => {
+});
 
 module.exports = app;
